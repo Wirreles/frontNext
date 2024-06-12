1556044d0c8fcc7f963299641b088f7dc3579cba
tomas.avila <tomysykess@gmail.com>
2024-06-12 02:01:45 -0300

    recommendation deploy

--- liqours/src/components/halfLanding/halfRecommendation/Halfrecomendation.tsx
+++ liqours/src/components/halfLanding/halfRecommendation/Halfrecomendation.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { IRecommendation } from "@/interfaces/interfaz";
 import React, { useState, useEffect, useRef } from "react";
-import Image from 'next/image';
 
 const HalfRecommendation = ({
   recommendation,
@@ -12,7 +11,7 @@ const HalfRecommendation = ({
     recommendation;
   const [isVisible, setIsVisible] = useState(false);
   const [scrollY, setScrollY] = useState(0);
-  const ref = useRef<HTMLDivElement | null>(null);
+  const ref = useRef(null);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -24,14 +23,13 @@ const HalfRecommendation = ({
       { threshold: 0.1 }
     );
 
-    const currentRef = ref.current;
-    if (currentRef) {
-      observer.observe(currentRef);
+    if (ref.current) {
+      observer.observe(ref.current);
     }
 
     return () => {
-      if (currentRef) {
-        observer.unobserve(currentRef);
+      if (ref.current) {
+        observer.unobserve(ref.current);
       }
     };
   }, []);
@@ -55,57 +53,39 @@ const HalfRecommendation = ({
         isVisible ? "opacity-100 transition-opacity duration-1000" : "opacity-0"
       }`}
     >
-      <div
+      <img
+        src={imageB}
+        alt=""
         className="absolute object-contain w-full h-[105%] z-0 opacity-95"
         style={{ transform: `translateY(-${scrollY * 0.1}px)` }}
-      >
-        <Image
-          src={imageB}
-          alt=""
-          layout="fill"
-          objectFit="contain"
-          quality={100}
-        />
-      </div>
-      <div
+      />
+      <img
+        src={imageP}
+        alt=""
         className="absolute object-contain w-full h-[105%] z-0 opacity-95"
         style={{
           transform: `translateY(-${scrollY * 0.3}px)`,
           transition: "transform 3s ease",
         }}
-      >
-        <Image
-          src={imageP}
-          alt=""
-          layout="fill"
-          objectFit="contain"
-          quality={100}
-        />
-      </div>
-      <div
+      />
+      <img
+        src={imageF}
+        alt=""
         className="absolute object-contain w-full h-[105%] z-0 opacity-95"
         style={{
           transform: `translateY(-${scrollY * 0.5}px)`,
           transition: "transform 1s ease",
         }}
-      >
-        <Image
-          src={imageF}
-          alt=""
-          layout="fill"
-          objectFit="contain"
-          quality={100}
-        />
-      </div>
+      />
       <div className="relative z-10 text-base">
         <h1
-          className="text-4xl mb-96 font-bold text-center"
+          className="text-4xl mb-96 font-plus-jakarta-sanss text-center"
           style={{ color: color }}
         >
           {title}
         </h1>
         <b>
-          <p className="mt-10 text-lg text-center" style={{ color: color }}>
+          <p className="mt-10 text-lg  text-center" style={{ color: color }}>
             {description}
           </p>
         </b>
